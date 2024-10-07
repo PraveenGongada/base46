12f76fa7dd57643de31d375d98d6c241e47afa8b
PraveenGongada <praveengongada0603@gmail.com>
2024-10-07 23:43:10 +0530

    Modified ayu_dark colors

--- lua/base46/themes/ayu_dark.lua
+++ lua/base46/themes/ayu_dark.lua
@@ -1,58 +1,58 @@
--- credits to original theme https://github.com/ayu-theme/ayu-vim (dark)
+-- credits to original theme https://github.com/ayu-theme/ayu-vim (dark) and NvChad/base46
 -- This is just a modified version of it
 
 local M = {}
 
 M.base_30 = {
-  white = "#ced4df",
-  darker_black = "#05080e",
-  black = "#0B0E14", --  nvim bg
-  black2 = "#14171d",
-  one_bg = "#1c1f25",
-  one_bg2 = "#24272d",
-  one_bg3 = "#2b2e34",
-  grey = "#33363c",
-  grey_fg = "#3d4046",
-  grey_fg2 = "#46494f",
-  light_grey = "#54575d",
-  red = "#F07178",
-  baby_pink = "#ff949b",
-  pink = "#ff8087",
-  line = "#24272d", -- for lines like vertsplit
-  green = "#AAD84C",
-  vibrant_green = "#b9e75b",
-  blue = "#36A3D9",
-  nord_blue = "#43b0e6",
-  yellow = "#E7C547",
-  sun = "#f0df8a",
-  purple = "#c79bf4",
-  dark_purple = "#A37ACC",
-  teal = "#74c5aa",
-  orange = "#ffa455",
-  cyan = "#95E6CB",
-  statusline_bg = "#12151b",
-  lightbg = "#24272d",
-  pmenu_bg = "#ff9445",
-  folder_bg = "#98a3af",
+  white = "#d3d7df", -- slightly muted white
+  darker_black = "#080c11",
+  black = "#0E1117", -- nvim bg, reduced vibrance
+  black2 = "#181b20",
+  one_bg = "#20242a",
+  one_bg2 = "#282c31",
+  one_bg3 = "#303439",
+  grey = "#3a3d42", -- less vibrant grey
+  grey_fg = "#43464b",
+  grey_fg2 = "#4c4f54",
+  light_grey = "#5a5d62",
+  red = "#D95C64", -- less vibrant red
+  baby_pink = "#f78d94",
+  pink = "#e57c83", -- less vibrant pink
+  line = "#282c31", -- for lines like vertsplit
+  green = "#8fb13d", -- further desaturated green
+  vibrant_green = "#9bc74a",
+  blue = "#2d88b7", -- further desaturated blue
+  nord_blue = "#3a96c1",
+  yellow = "#c5a938", -- further desaturated yellow
+  sun = "#d3c375",
+  purple = "#b58ae7", -- desaturated purple remains the same
+  dark_purple = "#9571b4",
+  teal = "#69b39d", -- less vibrant teal
+  orange = "#e3974e", -- less vibrant orange
+  cyan = "#7fc1ac", -- further desaturated cyan
+  statusline_bg = "#181b20",
+  lightbg = "#282c31",
+  pmenu_bg = "#e5863f", -- less vibrant pmenu
+  folder_bg = "#88929e",
 }
 
 M.base_16 = {
-  base00 = "#0B0E14",
-  base01 = "#1c1f25",
-  base02 = "#24272d",
-  base03 = "#2b2e34",
-  base04 = "#33363c",
-  base05 = "#c9c7be",
-  base06 = "#E6E1CF",
-  base07 = "#D9D7CE",
-  base08 = "#c9c7be",
-  base09 = "#FFEE99",
-  base0A = "#56c3f9",
-  base0B = "#AAD84C",
-  base0C = "#FFB454",
-  base0D = "#F07174",
-  base0E = "#FFB454",
-  base0F = "#CBA6F7",
+  base00 = "#0E1117", -- reduced vibrance for base00
+  base01 = "#20242a",
+  base02 = "#282c31",
+  base03 = "#303439",
+  base04 = "#3a3d42", -- consistent with less vibrant greys
+  base05 = "#cbc9c0",
+  base06 = "#e8e3d1",
+  base07 = "#dbd9d0",
+  base08 = "#cbc9c0",
+  base09 = "#f8e38e", -- less vibrant yellow
+  base0A = "#4690c4", -- further desaturated blue
+  base0B = "#8fb13d", -- same desaturated green as base_30
+  base0C = "#d89841", -- further desaturated orange
+  base0D = "#D95C64", -- same red as base_30
+  base0E = "#d89841", -- consistent orange
+  base0F = "#b995e7", -- desaturated purple remains the same
 }
 
 M.polish_hl = {
